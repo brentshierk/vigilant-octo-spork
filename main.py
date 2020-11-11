e3f9310025b34d2e57ec5d7dbf496f7c184d63c4
brentshierk <34776645+brentshierk@users.noreply.github.com>
2020-11-11 12:31:35 -0500

    redone hw

--- main.py
+++ main.py
@@ -1,54 +1,47 @@
 from random import randrange
 
-class BankAccount:
+class BankAccount():
+    routing_number =randrange(00000000,100000000)
+
     
 
     def __init__(self,full_name,):
         self.full_name = full_name
         self.account_number = randrange(00000000,100000000)
-        self.routing_number =randrange(00000000,100000000)
         self.balance = 0
     
-    def deposit(self,balance,amount):
-        new_balance = amount + balance 
+    def deposit(self,amount):
+        new_balance = amount + self.balance 
         self.balance = new_balance
-        #print(f'you complated your deposit of {amount}')
+        print(f'you complated your deposit of {amount}')
 
-    def withdraw(self,balance,amount):
-        new_balance = balance - amount
+    def withdraw(self,amount):
+        new_balance = self.balance - amount
         self.balance = new_balance
-    
-    def get_balance(self,balance):
-        print('your balance is :')
         return self.balance
     
-    def add_intrest(self,balance):
-        intrest = balance * 0.00083
-        new_balance = balance + intrest
+    def get_balance(self,):
+        print(f"your balance is :{self.balance}")
+        return self.balance
+    
+    def add_intrest(self):
+        intrest = self.balance * 0.00083
+        new_balance = self.balance + intrest
         self.balance = new_balance
-        print(balance)
+        return self.balance
     
     def print_receipt(self):
-        print(Brent_BankAccount.full_name)
-        print('account number:',Brent_BankAccount.account_number)
-        print('routing number:',Brent_BankAccount.routing_number)
-        print(Brent_BankAccount.balance)
+        print(self.full_name)
+        print('account number:',self.account_number)
+        print('routing number:',self.routing_number)
+        print(self.balance)
 
 Brent_BankAccount = BankAccount('Brent Shierk')
+stephanie = BankAccount('Stephanie')
 
-# Brent_BankAccount.deposit(20,0)
-# print(Brent_BankAccount.deposit(20,0))
-# Brent_BankAccount.print_receipt()
-# Brent_BankAccount.withdraw(Brent_BankAccount.balance,100)
-
-Brent_BankAccount.deposit(Brent_BankAccount.balance,100)
-print(Brent_BankAccount.balance)
-Brent_BankAccount.withdraw(Brent_BankAccount.balance,40)
-print(Brent_BankAccount.balance)
-Brent_BankAccount.add_intrest(Brent_BankAccount.balance)
-Brent_BankAccount.print_receipt()
-   
-
+Brent_BankAccount.deposit(200)
+Brent_BankAccount.withdraw(40)
+Brent_BankAccount.get_balance()
 
 
 
